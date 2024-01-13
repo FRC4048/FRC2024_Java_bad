76fcaa5b7462f39e4d1e37f05a687c9a7dbd6e1c
Lucas Vedana <lvdn58@icloud.com>
2024-01-13 08:20:06 -0800

    Here is my shooter code

--- FRC2024_Java/src/main/java/frc/robot/subsystems/Shooter.java
+++ FRC2024_Java/src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,30 +4,30 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Shooter extends SubsystemBase {
   
-  private final Spark shooterWheel1;
-  private final Spark shooterWheel2;
+  private final PWMSparkMax shooterWheel1;
+  private final PWMSparkMax shooterWheel2;
   private final DigitalInput shooterSensor;
 
   public Shooter() {
-    shooterWheel1 = new Spark(Constants.SHOOTER_MOTOR_ID_1);
-    shooterWheel2 = new Spark(Constants.SHOOTER_MOTOR_ID_2);
+    shooterWheel1 = new PWMSparkMax(Constants.SHOOTER_MOTOR_ID_1);
+    shooterWheel2 = new PWMSparkMax(Constants.SHOOTER_MOTOR_ID_2);
     shooterSensor = new DigitalInput(Constants.SHOOTER_SENSOR_ID);
   }
 
-  //Method to spin shooter motors
+  //Spin shooter motors
   public void spinMotors(double speed) {
     shooterWheel1.set(speed);
     shooterWheel2.set(speed);
   }
 
-  //Method to stop shooter motors
+  //Stop shooter motors
   public void stopMotor() {
     shooterWheel1.stopMotor();
     shooterWheel2.stopMotor();
